d0b35c5964ba2d8022b132a54a9d3cb485a08bd9
mateuscampos72 <mateuscampos72@gmail.com>
2023-02-11 13:56:34 -0300

    finalização

--- index.php
+++ index.php
@@ -27,21 +27,12 @@
                     <div class="card-body">
                         
                         <form id="geradorform" class="form">
-                            
-                                <!-- <h3 class="password-display" id="displaysenha">Sua senha aparecerá aqui<h3> -->
-                                
-                                <div class="copy-clipboard">
-                                    <div class="input-group mb-3">
                                     <input class="form-control" id="displaysenha" readonly      placeholder="Sua senha aparecerá aqui"><button class="input-group-text" onclick="copyfunc()"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg></button>
-                                </div>
-                            <div class="form-group">
                                 <div class="form-select">
                                     <label for="quantidadenumeros">Quantidade de caracteres</label>
                                     <input type="range" min="8" max="20" value="10"  class="form-range" id="quantidadeletras">
                                     <!-- <input type="number" class="form-control" min="8" max="20" value="10" id="quantidadenumeros"> -->
                                 <div class="input-group bootstrap-touchspin bootstrap-touchspin-injected"><span class="input-group-btn input-group-prepend"><button class="btn btn-primary bootstrap-touchspin-down" id="diminuirQuantidade" type="button">-</button></span><input type="number" class="form-control" min="8" max="20" value="10" id="demo3"><span class="input-group-btn input-group-append"><button class="btn btn-primary bootstrap-touchspin-up" id="aumentaQuantidade" type="button">+</button></span></div>
-                            </div>
-                        </div>
                             <div class="multiselecao mt-4">
                                 <div class="form-check form-check-inline">
                                     <input class="form-check-input success check-light-success" type="checkbox" id="incluirmaiusculas" value="option1" >
