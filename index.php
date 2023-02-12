3076db517da2d1408e4952ab5968ec67901901ab
mateusbertolo <113317452+mateusbertolo@users.noreply.github.com>
2023-02-12 19:51:02 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,50 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="style.css">
     <title>eClientTools</title>
+    <style>
+        .form-range::-webkit-slider-thumb{
+    -webkit-appearance: none;
+    appearance: none;
+    height:20px;
+    width:20px;
+    border-radius: 50%;
+    background: rgb(240, 7, 150);
+
+}
+.form-range::-moz-range-thumb{
+-webkit-appearance: none;
+    appearance: none;
+     
+}
+.b1{
+   padding: 5px;
+   font-weight: bold;
+   color: #00B4FF;
+}
+#marker{
+    position:relative;
+    right:0px;
+    height:20px;
+    width:30px;
+    z-index:100;
+}
+.fa{
+    font-size: 38px;
+    color: #00B4FF;
+    text-align: center;
+}
+#para{
+    height:20px;
+    width:30px;
+   margin-left: -23px; 
+    position: absolute;
+    padding:5px 0px;
+    font-weight: bold;
+}
+    </style>
 </head>
 <body>
     <div id="header">
@@ -32,12 +74,16 @@
                                 </div> 
                                 <div class="labelcar">
                                     <label for="quantidadenumeros">Quantidade de caracter:</label>
-                                </div>
+                                </div>   
+                                <div  id="marker"><i class="fa fa-map-marker"></i><span id="para">10</span></div>
                                     <div class="caracter">   
                                     <div id="rangeValue"><input type="number" readonly class="form-control" min="8" max="20" value="10" id="demo3"></div>
-                                    <!-- <input type="number" class="form-control" min="8" max="20" value="10" id="quantidadenumeros"> -->
-                                <div class="input-char"><button class="input-menos" id="diminuirQuantidade" type="button">-</button><input type="range" min="8" max="20" value="10"  class="form-range" id="quantidadeletras"><button class="input-mais" id="aumentaQuantidade" type="button">+</button></span></div>
-                           
+                                <div class="input-char"><button class="input-menos" id="diminuirQuantidade" type="button">-</button>
+                                <span class="b1">8</span>
+                                <input type="range" min="8" max="20" value="10"  class="form-range" id="quantidadeletras" oninput="prize()">
+                                <span class="b1">20</span>
+                                <button class="input-mais" id="aumentaQuantidade" type="button">+</button></span></div>
+                                    <div id="emoji"></div>
                                 <div class="labelcar">
                                     <label for="quantidadenumeros">Caracteres Ultilizados:</label>
                                 </div> 
@@ -63,8 +109,30 @@
                         
                     </div>
         <script src="gerador.js"></script>
+        <script>
+               var msg=document.getElementById("para");
+                    var SLI=document.getElementById("quantidadeletras");
+                    var val=document.getElementById("quantidadeletras").value;
+                    var MARKER=document.getElementById("marker");
+                    MARKER.style="left: calc(" + val.value + "% - 17px";
+                    MARKER.style.opacity="1";
+
+                    function prize(){
+                        var msg=document.getElementById("para");
+                        var val=document.getElementById("quantidadeletras").value;
+                        var MARKER=document.getElementById("marker");
+                        msg.innerText=val;
+                        MARKER.style="left: calc(" + val.value + "% - 17px";
+                        MARKER.style.opacity="1";
+                    }
+
+        </script>
 </body>
 </html>
+
+
+
+
 
 
 
